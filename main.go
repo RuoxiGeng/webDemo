e9761043ee57556ffa317ab3ca5afc440b232241
rugeng <rugeng@ebay.com>
2020-01-03 13:56:50 +0800

    router

--- main.go
+++ main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
-	"./gee"
+	"./gee1"
 )
 
+func onlyForV2() gee1.HandlerFunc {
+	return func(c *gee1.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 func main() {
-	r := gee.New()
-	r.GET("/", func(c *gee.Context) {
+	r := gee1.New()
+	r.Use(gee1.Logger()) // global midlleware
+	r.GET("/", func(c *gee1.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
 
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
+	{
+		v2.GET("/hello/:name", func(c *gee1.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-	})
+	}
 
 	r.Run(":9999")
 }
